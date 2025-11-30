bf3db5ba3d83eb0a508a8496b61356f1dae3f78c
Pedro Ribeiro <pedror@hellomolly.com>
2025-11-30 19:38:17 +1100

    Remove unused files

--- src/commands/command.module.ts
+++ src/commands/command.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ExampleCommand } from './example.command';
 import { HealthCommand } from './health.command';
-import { ReadCommand } from './read.command';
 import { ParseTransactionsCommand } from './parse-transactions.command';
 import { AppService } from '../app.service';
 import { ParserModule } from '../parsers/parser.module';
 
 @Module({
   imports: [ParserModule],
-  providers: [
-    ExampleCommand,
-    HealthCommand,
-    ReadCommand,
-    ParseTransactionsCommand,
-    AppService,
-  ],
+  providers: [HealthCommand, ParseTransactionsCommand, AppService],
 })
 export class CommandModule {}
